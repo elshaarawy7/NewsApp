b38338968df5e30236fb13824395e7741e9c4464
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-02 11:49:46 +0200

    edit mathod git news

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ void main() {
 
 final dio = Dio();
 
-void getHttp() async {
-  final response = await dio.get('https://dart.dev');
+void GetNews() async {
+  final response = await dio.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=77a414352ab44b418b41e8c335c9b2cd');
   print(response);
 }
 
