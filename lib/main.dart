5b8f228b15f0724382ad39ad314a6df606c30ce5
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-02 11:47:21 +0200

    create dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:news_app2/pages/homePage.dart';
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(const NewsApp());
+} 
+
+final dio = Dio();
+
+void getHttp() async {
+  final response = await dio.get('https://dart.dev');
+  print(response);
 }
 
 class NewsApp extends StatelessWidget {
